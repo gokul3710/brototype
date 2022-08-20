b3ce73e0c149c810d995d46c6db0f5163053ef30
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-08-20 19:51:00 +0530

    sworker update commit

--- sworker.js
+++ sworker.js
@@ -3,12 +3,17 @@ self.addEventListener("install", e=>{
     e.waitUntil(
         caches.open("static").then(cache=>{
             return cache.addAll([
-                // "./index.html",
-                // "./about.html",
-                // "./contact.html",
-                // "./genre.html",
-                // "./assets/css/style-liberty.css",
-                // "./assets/images/icon.png"
+                "./index.html",
+                "./about.html",
+                "./contact.html",
+                "./courses.html",
+                "./assets/css/style.css",
+                "./assets/js/main.js",
+                "./assets/images/icon.png",
+                "./assets/images/100k-challenge.jpg",
+                "./assets/images/web-development.jpg",
+                "./assets/images/flutter-challenge.jpg",
+                "./assets/images/Programming-pana.svg"
             ]);
         })
     )
