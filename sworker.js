ed540be3b8a84fc28f4f5cb904ccdd9eaa192d4e
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-08-20 20:03:34 +0530

    sworker update 2 commit

--- sworker.js
+++ sworker.js
@@ -8,12 +8,31 @@ self.addEventListener("install", e=>{
                 "./contact.html",
                 "./courses.html",
                 "./assets/css/style.css",
+                "./assets/js/about.js",
+                "./assets/js/courses.js",
+                "./assets/js/contact.js",
                 "./assets/js/main.js",
                 "./assets/images/icon.png",
+                "./assets/images/logo-theme-black.png",
+                "./assets/images/logo-white-black.png",
                 "./assets/images/100k-challenge.jpg",
                 "./assets/images/web-development.jpg",
                 "./assets/images/flutter-challenge.jpg",
-                "./assets/images/Programming-pana.svg"
+                "./assets/images/web-designing.jpg",
+                "./assets/images/git-challenge.jpg",
+                "./assets/images/python-challenge.jpg",
+                "./assets/images/data-structure.jpg",
+                "./assets/images/react-challenge.jpg",
+                "./assets/images/game-development.jpg",
+                "./assets/images/tech-tips.jpg",
+                "./assets/images/talk-shows.jpg",
+                "./assets/images/business-tips.jpg",
+                "./assets/images/boiler-room.jpg",
+                "./assets/images/web-hosting.jpg",
+                "./assets/images/live-sessions.jpg",
+                "./assets/images/Programming-pana.svg",
+                "./assets/images/contact.svg",
+                "./assets/images/about achievements.svg"
             ]);
         })
     )
