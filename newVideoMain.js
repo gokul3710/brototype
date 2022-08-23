b7045b049026eca84f34476e9a1669094929e569
gokul3710 <107317309+gokul3710@users.noreply.github.com>
2022-08-24 00:08:35 +0530

    Update newVideoMain.js

--- newVideoMain.js
+++ newVideoMain.js
@@ -1,8 +1,8 @@
     newVideos.forEach(newVideo => {
         const newVideoArticle = document.createElement('article');
         const newVideoArticleContent = `
-                                    <div class="course-image"><img src="${newVideo.Image}"></div>
-                                    <div class="course-info"><h4>${newVideo.Name}</h4><p>${newVideo.Details}</p><a href="${newVideo.Link}" class="btn btn-primary">Watch Video</a></div>
+                                    <div class="course-image"><img src="https://img.youtube.com/vi/${newVideo.Id}/maxresdefault.jpg"></div>
+                                    <div class="course-info"><h4>${newVideo.Name}</h4><p>${newVideo.Details}</p><a href="https://youtu.be/${newVideo.Id}" class="btn btn-primary">Watch Video</a></div>
                                     `;
     newVideoArticle.innerHTML = newVideoArticleContent;
     document.querySelector('.videos-container').appendChild(newVideoArticle);
