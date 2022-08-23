760a45ba40f51637ae109db5c7b787dff9d1ae76
gokul3710 <107317309+gokul3710@users.noreply.github.com>
2022-08-23 23:34:32 +0530

    Update newVideos.js

--- newVideos.js
+++ newVideos.js
@@ -3,25 +3,30 @@ const newVideos = [
         Image: './assets/images/tech-tips.jpg',
         Name: 'Tech & Career Tips',
         Details: 'Videos on Different IT Domains & Careers.',
+        Link: 'https://youtu.be/KQm-MDMGK2s',
     },
     {
         Image: './assets/images/tech-tips.jpg',
         Name: 'Tech & Career Tips',
         Details: 'Videos on Different IT Domains & Careers.',
+        Link: 'https://youtu.be/KQm-MDMGK2s',
     },
     {
         Image: './assets/images/tech-tips.jpg',
         Name: 'Tech & Career Tips',
         Details: 'Videos on Different IT Domains & Careers.',
+        Link: 'https://youtu.be/KQm-MDMGK2s',
     },
     {
         Image: './assets/images/tech-tips.jpg',
         Name: 'Tech & Career Tips',
         Details: 'Videos on Different IT Domains & Careers.',
+        Link: 'https://youtu.be/KQm-MDMGK2s',
     },
     {
         Image: './assets/images/tech-tips.jpg',
         Name: 'Tech & Career Tips',
         Details: 'Videos on Different IT Domains & Careers.',
+        Link: 'https://youtu.be/KQm-MDMGK2s',
     }
 ]
