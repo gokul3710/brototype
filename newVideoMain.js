e1cd96750a93c2d8c3576e137870233762435a38
gokul3710 <107317309+gokul3710@users.noreply.github.com>
2022-08-23 23:35:14 +0530

    Update newVideoMain.js

--- newVideoMain.js
+++ newVideoMain.js
@@ -2,7 +2,7 @@
         const newVideoArticle = document.createElement('article');
         const newVideoArticleContent = `
                                     <div class="course-image"><img src="${newVideo.Image}"></div>
-                                    <div class="course-info"><h4>${newVideo.Name}</h4><p>${newVideo.Details}</p><a href="" class="btn btn-primary">Watch Video</a></div>
+                                    <div class="course-info"><h4>${newVideo.Name}</h4><p>${newVideo.Details}</p><a href="${newVideo.Link}" class="btn btn-primary">Watch Video</a></div>
                                     `;
     newVideoArticle.innerHTML = newVideoArticleContent;
     document.querySelector('.videos-container').appendChild(newVideoArticle);
