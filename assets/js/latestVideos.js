e98092dbb35df7d54e7cdbf32279892d73aa48f4
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-09-21 14:18:13 +0530

    content updated commit

--- assets/js/latestVideos.js
+++ assets/js/latestVideos.js
@@ -25,4 +25,4 @@ function latestVideos(){
     });
 }
 
-latestVideos()
+latestVideos();
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,23 +5,6 @@ window.addEventListener("scroll",() => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 })
 
-// FAQs section
-
-const faqs = document.querySelectorAll(".faq");
-
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-
-        const icon = faq.querySelector('.faq-icon i');
-        if(icon.className === ('fa-solid fa-plus')){
-            icon.className = 'fa-solid fa-minus';
-        }else{
-            icon.className = 'fa-solid fa-plus'
-        }
-    })
-})
-
 // navbar in tablets and mobiles
 
 const nav = document.querySelector('.nav-menu');
@@ -51,3 +34,21 @@ if(document.body.clientWidth < 1024){
     navLink.addEventListener('click', closeNav);
   })
 }
+
+
+// FAQs section
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === ('fa-solid fa-plus')){
+            icon.className = 'fa-solid fa-minus';
+        }else{
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
+})
